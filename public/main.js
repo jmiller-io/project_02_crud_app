e8452c47b2fdcc248d59d3634bb5165a32c10fe4
Jake Miller <hillsighed@gmail.com>
2017-01-17 11:18:07 -0800

    adds functionality to pull from DB and auto render the results

--- public/main.js
+++ public/main.js
@@ -3,51 +3,64 @@ console.log('hi from main')
   // My coordinates
   //34.154413, -118.120233
 
+
   // Database schema
-var structures = [
-  {Description: 'Home',
-   coordinates: {lat: 34.154413, lng: -118.120233},
-   imgLocation: 'public/images',
-   category: 'bungalow'
-  },
-  {Description: 'Away',
-   coordinates: {lat: 34.1, lng: -118.1},
-   imgLocation: 'public/images',
-   category: 'bungalow'
-  }
-];
+// var structures = [
+//   {Description: 'Home',
+//    coordinates: {lat: 34.154413, lng: -118.120233},
+//    imgLocation: 'public/images',
+//    category: 'bungalow'
+//   },
+//   {Description: 'Away',
+//    coordinates: {lat: 34.1, lng: -118.1},
+//    imgLocation: 'public/images',
+//    category: 'bungalow'
+//   }
+// ];
 
 
-$.get('/data.json', function( response ) {
-  var markerData = JSON.stringify(response);
-});
+// var getGeoData = $.get('/data.json', function( response ) {
+//     markerData = response;
+//     return markerData
+//   });
 
+// var markerData;
 
+// $.get('/data.json', function( response ) {
+//     markerData = response;
+//     return markerData
+//   });
 
 
 function initMap() {
+
+  // Create the Map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.154413, lng: -118.120233},
     zoom: 14
   });
 
+  // Get all the data from the Database
+  $.get('/data.json', function( response ) {
+    markerData = response;
 
-  structures.forEach(function(obj) {
-    var marker = new google.maps.Marker({
-      position: obj.coordinates,
-      map: map,
-      title: 'Hello World!' //what displays when mouse over
-
-    });
-
-    var infowindow = new google.maps.InfoWindow({
-        content: '<img src="img_uploads/example_bungalow.jpeg"><br /> <p>Beautiful Craftsman Bungalow in Pasadena.</p><p>Category: <i>Bungalow</i></p>' // in the windo
+    // iterate through objects creates map marker
+    markerData.forEach(function(obj) {
+      var marker = new google.maps.Marker({
+        position: obj.coordinates,
+        map: map,
+        title: 'Hello World!' //what displays when mouse over
       });
 
+      // Create info window with object database info
+      var infowindow = new google.maps.InfoWindow({
+        content: '<img src="' + obj.imgLocation + '"><br /> <p>' + obj.description + '</p><p>Category: <i>' + obj.category + '</i></p>' // in the windo
+      });
+
+      // info window event listener
       marker.addListener('click', function() {
-      infowindow.open(map, marker);
+        infowindow.open(map, marker);
       });
-
-  })
-
+    });
+  });
 };
