20cf471565f7b3d14edb9fff74912053bd0515d4
Jake Miller <hillsighed@gmail.com>
2017-03-31 18:57:03 -0700

    sends user data to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -5,14 +5,15 @@ const user = require('../models/user.js');
 
 router.get('/', (req, res, next) => {
     const user = req.session.user;
+    console.log(user)
     if (!user) return res.redirect('/');
 
-    // res.render( profile page )
-    res.send(`<h1>${f_name}</h1><img src=${avatar_url}>`);
+    res.render('profilePage', {title: 'Profile - Architecturally', user: user})
+
 });
 
 router.get('/me', (req, res, next) => {
-    const url = 'https://www.googleapis.com/plus/v1/people/me';
+    const url = 'https://people.googleapis.com/v1/people/me';
     const access_token = req.session.access_token;
     if (!access_token) return res.redirect('/');
     const options = {
@@ -25,6 +26,7 @@ router.get('/me', (req, res, next) => {
   request(options, (err, response, body) => {
     const userData = JSON.parse(body);
     req.session.user = userData;
+    console.log(userData.displayName)
     // Database work
     user.findById({
         _id: req.session.user.id
@@ -32,16 +34,16 @@ router.get('/me', (req, res, next) => {
         if (err) console.log(err)
         if (!results) {
           console.log('no existing user in db');
-            var u = new user({
-              name: req.session.user.name.givenName,
-              avatar: req.session.user.image.url
-            });
-            u.save();
+            // var u = new user({
+            //   name: req.session.user.name.givenName,
+            //   avatar: req.session.user.image.url
+            // });
+            // u.save();
             } else if (results) {
               console.log('user exists in db');
-              req.session.user.locations = results.locations;
+              // req.session.user.locations = results.locations;
             }
-            return res.redirect('/');
+            return res.redirect('/profile');
         });
     });
 });
